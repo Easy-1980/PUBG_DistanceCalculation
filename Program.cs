95b13673609425d47b6abdcf2eb6027ae180d654
Easy-1980 <3211618922@qq.com>
2025-12-25 20:59:58 +0800

    项目代码

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using PUBG_CalculateDistance;
+
+class Program
+{
+    // 引入系统 API 来设置 DPI 感知
+    [DllImport("user32.dll")]
+    private static extern bool SetProcessDPIAware();
+
+    static async Task Main(string[] args)
+    {
+
+        SetProcessDPIAware();
+
+        Console.WriteLine(" PUBG 测距工具 ");
+        Console.WriteLine("按住 Left CTRL 开始测距...");
+
+        // 启动 Overlay
+        using var overlay = new PUBG_Overlay();
+        await overlay.Run();
+    }
+}
